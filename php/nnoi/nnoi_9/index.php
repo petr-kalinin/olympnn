be5e515758553a530255d30936a028fe17586aff
Petr <petr@kalinin-lenovo.(none)>
2013-01-31 21:51:56 +0400

    NNOI9 raw archive added

---
+++ php/nnoi/nnoi_9/index.php
@@ -0,0 +1,31 @@
+<?
+$backpath="../..";
+include_once("$backpath/onn.php");
+startonn("/Город/NNOI IX","IX Нижегородская городская олимпиада школьников по информатике");
+?>
+<h1>IX Нижегородская городская олимпиада школьников по информатике</h1>
+<p>
+IX Нижегородская городская олимпиада школьников по информатике была проведена 31 января 2013&nbsp;г. 
+на трех площадках одновременно: <a href="http://ipfran.ru/training/training_center.html">НОЦ ИПФ
+РАН</a>, <a href="http://www.unn.ru/">ННГУ</a> и <a href="http://lic40.nnov.ru/">лицей 40</a>. 
+В олимпиаде приняли участие 61 участник, 16 из которых были награждены дипломами.
+</p>
+<p>Спонсором олимпиады выступила <a href="http://www.meranetworks.ru/">компания Mera</a>.</p>
+<?/*<p>Условия задач, разборы, примеры решений и другие материалы олимпиады (c) 2010-2011 <a href="taskauthors.php">Авторы задач 
+олимпиады</a>.</p>*/?>
+</p>
+<ul>
+<?/*<li><a href="taskauthors.php">Авторы задач</a></li>
+<li><a href="jury.php">Жюри</a></li>
+<li><a href="org.php">Оргкомитет</a></li>
+</ul>
+<ul>
+<li><a href="results.php">Результаты</a></li>*/?>
+<li><a href="archive/index.php">"Сырой" архив</a></li>
+</ul>
+<?/*<ul>
+<li><a href="http://informatics.mccme.ru/moodle/mod/statements/view.php?id=4543">Online-проверка</a></li>
+</ul>*/?>
+<?
+endonn();
+?>
