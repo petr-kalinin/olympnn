ef7132c13c33f3677c42093111000dd5433d2a13
Petr <petr@kalinin-lenovo.(none)>
2013-02-03 21:34:35 +0400

    News added

--- php/news/news.php
+++ php/news/news.php
@@ -7,7 +7,7 @@ function shownews($day){
   global $news;
   write("<li><b>".date("d.m.Y",$day)."</b>: {$news[$day]["text"]}");
   if (isset($news[$day]["href"])){
-     write(" <a href=\"/news/{$news[$day]["href"]}\"><i>подробнее&nbsp;&gt;&gt;&gt;</i></a>");
+     write(" <a href=\"{$news[$day]["href"]}\"><i>подробнее&nbsp;&gt;&gt;&gt;</i></a>");
   }
   writeln("</li>");
 }
