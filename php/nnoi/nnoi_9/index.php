5e3b9344fb405586c794cb06beba51e12fee1e79
Petr Kalinin <petr@kalinin.nnov.ru>
2013-10-16 21:31:13 +0400

    Updated index.php for NNOI9

--- php/nnoi/nnoi_9/index.php
+++ php/nnoi/nnoi_9/index.php
@@ -11,21 +11,21 @@ IX Нижегородская городская олимпиада школьников по информатике была проведена 31
 В олимпиаде приняли участие 63 участника, 19 из которых были награждены дипломами.
 </p>
 <p>Спонсором олимпиады выступила <a href="http://www.meranetworks.ru/">компания Mera</a>.</p>
-<?/*<p>Условия задач, разборы, примеры решений и другие материалы олимпиады (c) 2010-2011 <a href="taskauthors.php">Авторы задач 
-олимпиады</a>.</p>*/?>
+<p>Условия задач, разборы, примеры решений и другие материалы олимпиады (c) 2012-2013 <a href="taskauthors.php">Авторы задач 
+олимпиады</a>.</p>
 </p>
 <ul>
-<?/*<li><a href="taskauthors.php">Авторы задач</a></li>
+<li><a href="taskauthors.php">Авторы задач</a></li>
 <li><a href="jury.php">Жюри</a></li>
 <li><a href="org.php">Оргкомитет</a></li>
 </ul>
 <ul>
-<li><a href="results.php">Результаты</a></li>*/?>
+<li><a href="results.php">Результаты</a></li>
 <li><a href="archive/index.php">"Сырой" архив</a></li>
 </ul>
-<?/*<ul>
+<ul>
 <li><a href="http://informatics.mccme.ru/moodle/mod/statements/view.php?id=4543">Online-проверка</a></li>
-</ul>*/?>
+</ul>
 <?
 endonn();
 ?>
