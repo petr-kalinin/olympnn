a757fa3474f162f0d57b669a52db450f6718f2f0
Petr Kalinin <petr@kalinin.nnov.ru>
2015-01-31 19:22:36 +0300

    Fixed stats

--- php/nnoi/nnoi_11/stat.php
+++ php/nnoi/nnoi_11/stat.php
@@ -10,7 +10,7 @@ startonn("/Город/NNOI XI/Результаты","Статистика XI Нижегородской городской олим
 if (isset($_GET["prob"])) {
    loadxml("results/results.xml",$results);
    if (($_GET["prob"]=='all')or isset($results["results"]["problems"][$_GET["prob"]]))
-      ije_showstat("nnoi/nnoi_10/results/results.xml",$_GET["prob"]);
+      ije_showstat("nnoi/nnoi_11/results/results.xml",$_GET["prob"]);
    else writeln("Задача ".htmlspecialchars($_GET["prob"])." не найдена...");
 } else writeln("Задача не найдена...");
 endonn();
