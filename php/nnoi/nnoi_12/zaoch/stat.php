50d3d26ace436d269df03ed2e377efff5f673881
Petr Kalinin <petr@kalinin.nnov.ru>
2016-01-30 17:17:21 +0300

    zaoch results

---
+++ php/nnoi/nnoi_12/zaoch/stat.php
@@ -0,0 +1,17 @@
+<?
+$backpath="../../..";
+include_once("$backpath/onn.php");
+include_once("$backpath/ije_proc.php");
+startonn("/Город/NNOI XII/Отборочный тур","Статистика отборочного тура XII Нижегородской городской олимпиады",array("/style.css","/ije_proc.css"));
+?>
+<h1>Отборочный тур XII Нижегородской городской олимпиады школьников по информатике</h1>
+<h2>Статистика</h2>
+<?
+if (isset($_GET["prob"])) {
+   loadxml("results/results.xml",$results);
+   if (($_GET["prob"]=='all')or isset($results["results"]["problems"][$_GET["prob"]]))
+      ije_showstat("nnoi/nnoi_12/zaoch/results/results.xml",$_GET["prob"]);
+   else writeln("Задача ".htmlspecialchars($_GET["prob"])." не найдена...");
+} else writeln("Задача не найдена...");
+endonn();
+?>
