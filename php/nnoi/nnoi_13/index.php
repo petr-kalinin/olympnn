74e4151690fe477af6d6da2b780d5eef518f2c41
Petr Kalinin <petr@kalinin.nnov.ru>
2017-01-31 22:09:02 +0300

    Added info about zaoch

--- php/nnoi/nnoi_13/index.php
+++ php/nnoi/nnoi_13/index.php
@@ -9,8 +9,12 @@ XIII Нижегородская городская олимпиада школьников по информатике им. В.&nbsp;Д. 
 в <a href="http://www.unn.ru/">Нижегородском государственном университете</a>.
 </p>
 <p>Спонсорами олимпиады выступят <a href="http://www.prima.nnov.ru/">НПП Прима</a>, <a href="http://yandex.ru/">компания Яндекс</a> и <a href="http://unn.ru">ННГУ</a>.</p>
-<p>Подробная информация об олимпиаде в <a href="nnoi2017_prikaz.pdf">приказе</a>, в частности, информация об отборе на олимпиаду содержится в приложении 3 к приказу. 
-Для подачи заявки на участие в олимпиаде необходимо заполнить <a href="https://docs.google.com/a/kalinin.nnov.ru/forms/d/e/1FAIpQLSfimIZn2ATfiI1FKo2NAELhbSeO726EBD87_XedhyEdfkK-AA/viewform">регистрационную форму</a>.
+<!--<p>Подробная информация об олимпиаде в <a href="nnoi2017_prikaz.pdf">приказе</a>, в частности, информация об отборе на олимпиаду содержится в приложении 3 к приказу. 
+Для подачи заявки на участие в олимпиаде необходимо заполнить <a href="https://docs.google.com/a/kalinin.nnov.ru/forms/d/e/1FAIpQLSfimIZn2ATfiI1FKo2NAELhbSeO726EBD87_XedhyEdfkK-AA/viewform">регистрационную форму</a>.-->
+<ul>
+<li><a href="nnoi2017_prikaz.pdf">Приказ о проведении олимпиады</a></li>
+<li><a href="zaoch.php">Информация о рассмотрении заявок на участие</a></li>
+</ul>
 <div class="logos">
 <a href="http://yandex.ru"><img src="https://avatars.mds.yandex.net/get-bunker/60661/5ec62cb755193c37a6ec19a826b3891780eead2a/orig"/></a>
 <a href="http://www.prima.nnov.ru"><img src="prima.png"/></a>
