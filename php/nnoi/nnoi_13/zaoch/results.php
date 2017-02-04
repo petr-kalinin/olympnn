01b976b3affa8353dcf2ade6371b706e60e41def
Petr Kalinin <petr@kalinin.nnov.ru>
2017-02-04 15:30:43 +0300

    Added zaoch results, no direct link yet

---
+++ php/nnoi/nnoi_13/zaoch/results.php
@@ -0,0 +1,14 @@
+<?
+$backpath="../../..";
+include_once("$backpath/onn.php");
+include_once("$backpath/ije_proc.php");
+startonn("/Город/NNOI XIII/Отборочный тур","Результаты отброчного тура XIII Нижегородской городской олимпиады",array("/style.css","/ije_proc.css"));
+?>
+<h1>XII Нижегородская городская олимпиада школьников по информатике</h1>
+<h2>Результаты отборочного тура</h2>
+<p>По всем вопросам, связанным с результатами и тестированием, можно писать Петру Калинину на petr 
+AT kalinin ТОЧКА nnov ТОЧКА ru.<p>
+<?
+ije_showresults("results/results.xml","results/addinfo.txt",true,"reports.php","results/more_data.xml");
+endonn();
+?>
