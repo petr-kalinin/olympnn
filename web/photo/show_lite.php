d83394b5ec6e83be8303f301c95b855bff71c4e3
Petr Kalinin <petr@kalinin.nnov.ru>
2017-11-11 21:55:25 +0300

    Fixed <?php = in one more place

--- web/photo/show_lite.php
+++ web/photo/show_lite.php
@@ -29,7 +29,7 @@ if (!isset($index[$num])){
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
 <head>
-<title><?php ="Ôîòî $num &#8212; $index[title]"?></title>
+<title><?="Ôîòî $num &#8212; $index[title]"?></title>
 <link rel="stylesheet" href="show_lite.css"/>
 </head>
 <body>
