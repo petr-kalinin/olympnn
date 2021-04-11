b75e7ec39af263d8eca2c14a63dbbd261c3a6362
Petr Kalinin <petr@kalinin.nnov.ru>
2021-04-11 18:21:00 +0300

    Added nnoi 2021 general info

---
+++ web/nnoi/nnoi_17/index.php
@@ -0,0 +1,44 @@
+<?php 
+$backpath="../..";
+include_once("$backpath/onn.php");
+startonn("/Город/NNOI XVII","XVII Нижегородская олимпиада школьников по информатике");
+?>
+<h1>XVII Нижегородская олимпиада школьников по информатике</h1>
+<p>
+XVII Нижегородская олимпиада школьников по информатике им. В.&nbsp;Д. Лелюха состоится 15 мая 2021&nbsp;г. 
+в <a href="http://www.unn.ru/">Нижегородском государственном университете</a>.
+</p>
+<p>Спонсорами олимпиады выступят <a href="http://www.prima.nnov.ru/">НПП Прима</a>, <a href="http://yandex.ru/">компания Яндекс</a> и <a href="http://unn.ru">ННГУ</a>.</p>
+<ul>
+<li><a href="general.php">Общая информация о проведении олимпиады и порядок подачи заявок</a></li>
+<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdYx81ebP34DCZwkX0BTTnXJOakozK2Mlq9qIi_BzHBRbZTDQ/viewform">Форма подачи заявки</a></li>
+</ul>
+<?php /*<h2>Основной тур</h2>
+<ul>
+<li><a href="info.php">Информация об основном туре</a></li>
+<li><a href="participants.php">Итоговые списки участников основного тура</a></li>
+<li><a href="jury.php">Жюри</a></li>
+<li><a href="org.php">Оргкомитет</a></li>
+<li><a href="org.php">Авторы задач</a></li> 
+<li><a href="results.php">Результаты</a></li>
+<li><a href="archive/index.php">Архив</a></li>
+</ul>
+<h2>Отбор</h2>
+<ul>
+<li><a href="zaoch.php">Информация о рассмотрении заявок на участие</a></li>
+</li>
+</ul>
+<ul>
+<li><a href="zaoch/nnoi2020_zaoch_statements.pdf">Условия отборочного тура</a></li>
+<li><a href="zaoch/results.php">Результаты отборочного тура</a></li>
+<li><a href="zaoch/tests.7z">Тесты к отборочному туру</a></li>
+<li><a href="zaoch/outputs.7z">Протоколы тестирования отборочного тура</a></li>
+</ul>*/?>
+<div class="logos">
+<a href="http://yandex.ru"><img src="https://avatars.mds.yandex.net/get-bunker/60661/5ec62cb755193c37a6ec19a826b3891780eead2a/orig"/></a>
+<a href="http://www.prima.nnov.ru"><img src="prima.png"/></a>
+<a href="http://unn.ru"><img src="http://www.unn.ru/site/images/brand/unn_logo_ru.png"/></a>
+</div>
+<?php 
+endonn();
+?>
