65c246ab8273490970385eec30db1f18ca9e69ae
Petr Kalinin <petr@kalinin.nnov.ru>
2022-05-24 20:58:42 +0300

    Added nnoi 18 participants

--- web/nnoi/nnoi_18/index.php
+++ web/nnoi/nnoi_18/index.php
@@ -10,34 +10,26 @@ XVIII Нижегородская олимпиада школьников по информатике им. В.&nbsp;Д. Лелюха со
 </p>
 <ul>
 <li><a href="general.php">Общая информация о проведении олимпиады и порядок подачи заявок</a></li>
-<li><a href="https://forms.gle/xsjkM3hWC6yts5ro9">Форма подачи заявки</a></li>
+<?php /* <li><a href="https://forms.gle/xsjkM3hWC6yts5ro9">Форма подачи заявки</a></li> */ ?>
 </ul>
-<?php /*
 <h2>Основной тур</h2>
 <ul>
-<li><a href="info.php">Информация об основном туре</a></li>
+<?php /* <li><a href="info.php">Информация об основном туре</a></li> */ ?>
 <li><a href="participants.php">Итоговые списки участников основного тура</a></li>
-<li><a href="taskauthors.php">Авторы задач</a></li> 
+<?php /* <li><a href="taskauthors.php">Авторы задач</a></li> 
 <li><a href="results.php">Результаты</a></li>
-<li><a href="archive/index.php">Архив</a></li>
+<li><a href="archive/index.php">Архив</a></li> */ ?>
 </ul>
 <h2>Отбор</h2>
 <ul>
-<li><a href="zaoch.php">Информация о рассмотрении заявок на участие</a></li>
-</li>
-</ul>
-
-<ul>
-<li><a href="zaoch/nnoi2021_zaoch_statements.pdf">Условия отборочного тура</a></li>
-<li><a href="zaoch/results.php">Предварительные результаты отборочного тура</a></li>
-<li><a href="zaoch/tests.7z">Тесты к отборочному туру</a></li>
-<li><a href="zaoch/outputs.7z">Протоколы тестирования отборочного тура</a></li>
+<?php /* <li><a href="zaoch/nnoi2021_zaoch_statements.pdf">Условия отборочного тура</a></li> */ ?>
+<li><a href="https://nnoi2022.contest.codeforces.com/group/kWyGXwIvYI/contest/382707/standings">Результаты отборочного тура</a></li>
+<?php /* <li><a href="zaoch/tests.7z">Тесты к отборочному туру</a></li>
+<li><a href="zaoch/outputs.7z">Протоколы тестирования отборочного тура</a></li> */ ?>
 </ul>
 <div class="logos">
-<a href="http://yandex.ru"><img src="https://avatars.mds.yandex.net/get-bunker/60661/5ec62cb755193c37a6ec19a826b3891780eead2a/orig"/></a>
-<a href="http://www.prima.nnov.ru"><img src="prima.png"/></a>
 <a href="http://unn.ru"><img src="http://www.unn.ru/site/images/brand/unn_logo_ru.png"/></a>
-</div>*/?>
+</div>
 <?php 
 endonn();
 ?>
